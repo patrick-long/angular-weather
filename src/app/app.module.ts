3c6a90c59f41c4a00db8d120ade3c2391c6f9698
longpatrick <long.patrick1997@gmail.com>
2021-05-21 16:48:08 -0500

    generate weather component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { WeatherComponent } from './components/weather/weather.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
