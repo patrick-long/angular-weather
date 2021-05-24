1b296fb08dd02fe72f9c7bbac3b14e5bfa123fff
longpatrick <long.patrick1997@gmail.com>
2021-05-24 10:38:28 -0500

    complete simple routing

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
+  apiURL:string = 'http://api.weatherstack.com/forecast?access_key=9c992cd0c87f666755176a72842b6bd6&units=f&query=houston';
 
   constructor() { }
 
